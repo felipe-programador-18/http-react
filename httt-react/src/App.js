db9147d37a890ecf574555e34c412f94d00dd1e5
Felipe <machadofelipe2016@outlook.com>
2022-06-25 12:04:04 -0300

    c2-adding someoone code

--- httt-react/src/App.js
+++ httt-react/src/App.js
@@ -1,10 +1,71 @@
-
+import React,{useState, useEffect} from 'react'
 import './App.css';
 
-function App() {
-  return (
+const url ='http://localhost:3000/products'
+
+ function App() {
+  const [products, Setproducts] = useState([])
+  const [price, Setprice] = useState('')
+  const [name, Setnome] = useState('')
+
+  //1 rescuers data!!
+  useEffect( () => {
+     const FetchingDate = async() =>{
+     const res = await fetch(url)
+     const data = await res.json()
+     Setproducts(data)
+    }
+    FetchingDate()
+  }, []);
+  console.log(products)
+  
+ //2-adding products!!
+
+ const handSubmi = async(e) => {
+    e.preventDefault()
+    const product = {
+      name, 
+      price
+    };
+    //headers are requested
+    const res = await fetch(url, {
+      method:"POST",
+      headers:{
+       "Content-Type":'application/json'
+      },
+      body: JSON.stringify(product),
+    })
+   // const data = await res.json()
+    //Setproducts(data)
+ }
+
+
+ 
+ 
+  return ( 
     <div className="App">
-     <h1>Testing here now!!</h1>
+     <h1>list of products!!</h1>
+     {products.map((produts) => (<li key={produts.id} > {produts.nome} - R$:{produts.price} </li>))}
+    
+    
+      <div className='add-products' >
+        <h1>Adding products !!! </h1>
+        <form onSubmit={handSubmi} >
+          <label>
+            Nome:
+           <input type='text'  value={name} name='name' onChange={(e)=> Setnome(e.target.value)} />
+          
+          </label>
+          <label>
+            Price:
+           <input type='text'  value={price} name='name' onChange={(e)=> Setprice(e.target.value)} />
+          </label>
+          <input type='submit'  value='criar produto'/>
+
+        </form>
+      </div>
+    
+    
     </div>
   );
 }
